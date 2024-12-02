9a3d0c53a5387b1a694126a215c379f9996126d1
saddam <62154427+saddamlakho@users.noreply.github.com>
2024-12-02 09:08:59 +0500

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,3 @@
-import '../styles/globals.css';
 
          'use client'
          import Form from "./form";         
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+  'use client'
+   
+   import '../styles/globals.css';
+   import Login from "./login/form"
 
 
-'use client'
-import Login from "./login/form"
-export default function Home() {
-  
-return (
-    <div>
-      <link rel="icon" href="/favicon.ico" />
+   export default function Home() {
+   return (
+   <div>
+   <link rel="icon" href="/favicon.ico" />
       <Login/>
     
     </div>
